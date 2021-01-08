d87d3fe4899c27d7a7e60d8b4d61e799d9da8640
Patrick Winters <61561933+19wintersp@users.noreply.github.com>
2021-01-08 22:44:04 +0000

    Add files via upload

---
+++ src/com/_19wintersp/http/Method.java
@@ -0,0 +1,10 @@
+package com._19wintersp.http;
+
+public enum Method {
+	ALL,
+	GET,
+	POST,
+	PUT,
+	DELETE,
+	OTHER
+}
---
+++ src/com/_19wintersp/http/RouterPoint.java
@@ -0,0 +1,7 @@
+package com._19wintersp.http;
+
+public enum RouterPoint {
+	PREROUTE,
+	POSTROUTE,
+	ERROR
+}
